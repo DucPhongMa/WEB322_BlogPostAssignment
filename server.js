5b4f81f3a9d221792c57463e6b46ee8685fb236a
maducphong7@gmail.com <maducphong1998>
2022-06-03 14:43:13 -0400

    updated

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB322 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: _____Duc Phong Ma____Student ID: __137015194__ Date: __June 3rd, 2022_
+*
+*  Online (Heroku) URL: https://agile-savannah-70325.herokuapp.com/
+*
+********************************************************************************/ 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var path = require('path');
