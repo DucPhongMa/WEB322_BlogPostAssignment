ecbf96c9d59595dfadbb1d50c19e947fcc055950
maducphong7@gmail.com <maducphong1998>
2022-08-10 17:01:08 -0400

    updated assignment 6

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: _____Duc Phong Ma____Student ID: __137015194__ Date: __August 03, 2022_
+*  Name: _____Duc Phong Ma____Student ID: __137015194__ Date: __August 10, 2022_
 *
 *  Online (Heroku) URL: https://agile-savannah-70325.herokuapp.com/
 *
